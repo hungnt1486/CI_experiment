b249bb70fef9f87fe9eb06c37921d3e6478cf1d8
Lê Minh Hùng <hung@modmo.io>
2022-04-13 10:56:54 +0700

    create project

---
+++ CI_experiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CI_experiment
+//
+//  Created by Lê Hùng on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
